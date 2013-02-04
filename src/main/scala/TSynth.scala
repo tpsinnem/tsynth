1e3a0fe980ca213a4f12ceeb203936850b78bcd0
Timo Sinnemäki <timo.sinnemaki@helsinki.fi>
2013-02-04 10:14:36 +0200

    confusions cleared, early CircularArrayFilter, more ponderly todo notes added

--- src/main/scala/TSynth.scala
+++ src/main/scala/TSynth.scala
@@ -5,6 +5,13 @@ import scalaz.effect.ST
 import shapeless.{HList, UnaryTCConstraint}
 //import scalaz.effect.STArray
 
+//TODO  i will need to figure out if and where and how i'll need laziness to facilitate cyclic references
+//  - From what i gather, source/sources etc need to be declared lazy*, and the parameters for them need be
+//    introduced under a different name as nullary function values ('by name?').
+//   *- rather, i think, they need to _not be (regular) vals_?
+//    - for i moment i had them as lazy vals already on the base traits but i'll keep them as defs for now 
+//      because i don't want to restrict myself and i don't think this will ultimately cause any trouble
+
 // TODO also have a system with free sinks. Possibly also explicit free sources. Use shapeless.HList?
 // - hm seems like i'll need HList anyhow just for basic functionality
 trait TBasicSystem[Sources <: HList, Sinks <: HList] {
@@ -25,6 +32,8 @@ trait TNode {
 
   def deploy[S]:ST[S, NodeImpl[S]]
 
+  //TODO !!!! need to figure out how to responsibly [get data out of these]
+  //      - so far, all the methods in these are non-ST-monadic! -- and, thankfully, private.
   trait TNodeImpl[S] {
     private[tsynth] def operate:Unit
   }
@@ -32,8 +41,7 @@ trait TNode {
 
 trait TSource[ElemType] extends TNode {
 
-  type NodeImpl[S] <: SourceImpl[S] //TODO  there has to be some confusion behind this?
-  type SourceImpl[S] <: TSourceImpl[S]
+  type NodeImpl[S] <: TSourceImpl[S]
 
   trait TSourceImpl[S] extends TNodeImpl[S] {
     private[tsynth] def value:ElemType
@@ -42,8 +50,7 @@ trait TSource[ElemType] extends TNode {
 
 trait TSinkBase extends TNode {
   
-  type NodeImpl[S] <: SinkImpl[S] //TODO  see above?
-  type SinkImpl[S] <: TSinkBaseImpl[S]
+  type NodeImpl[S] <: TSinkBaseImpl[S]
 
   trait TSinkBaseImpl[S] {
 
@@ -54,9 +61,10 @@ trait TSinkBase extends TNode {
 
 trait TSink[ElemType] extends TSinkBase {
 
-  type SinkImpl[S] <: TSinkImpl[S]
+  type NodeImpl[S] <: TSinkImpl[S]
 
   def source:TSource[ElemType]
+
   trait TSinkImpl[S] extends TSinkBaseImpl[S] {
     private[tsynth] def source:TSink.source.NodeImpl[S]
     private[tsynth] var lastRead:ElemType
@@ -66,25 +74,21 @@ trait TSink[ElemType] extends TSinkBase {
 
 trait TFilterBase[Out] extends TSource[Out] with TSinkBase {
 
-  type NodeImpl[S] <: SourceImpl[S] with SinkImpl[S]
-  type SourceImpl[S] <: TFilterBaseImpl[S]  //TODO  see above again. confusion.
-  type SinkImpl[S] <: TFilterBaseImpl[S]
+  type NodeImpl[S] <: TFilterBaseImpl[S]
 
   trait TFilterBaseImpl[S] extends TSourceImpl[S] with TSinkBaseImpl[S]
 }
 
 trait TFilter[In, Out] extends TSink[In] with TFilberBase[Out] {
 
-  type SourceImpl[S] <: TFilterImpl[S]  //TODO  and again
-  type SinkImpl[S] <: TFilterImpl[S]
+  type NodeImpl[S] <: TFilterImpl[S]
 
   trait TFilterImpl[S] extends TSinkImpl[S] with TFilterBaseImpl[S]
 }
 
 trait TMixer2[In1, In2, Out] extends TFilterBase[Out] {
 
-  type SourceImpl[S] <: TMixer2Impl[S]  //TODO  and again
-  type SinkImpl[S] <: TMixer2Impl[S]
+  type NodeImpl[S] <: TMixer2Impl[S]
 
   def sources:(TSource[In1], TSource[In2])
 
@@ -102,8 +106,38 @@ case class VarTuple2[T1,T2](var _1:T1, var _2:T2)
 
 
 //trait or abstract class or such?
+//TODO keep remembering that this is an initial implementation and will need to be rethought!
 trait CircularArrayFilter[ElemType] extends TFilter[ElemType, ElemType] {
-  //TODO  everything 
+
+  type NodeImpl[S] <: CircularArrayFilterImpl[S]
+
+  val length:Int //TODO think: does this even belong here. shouldn't i instead have a [Seq[?]] member?
+                  //    - a seq or somesuch member with a length i can refer to.
+  val headUpdate: (ElemType, Array[ElemType], Int, Int) => ElemType
+  //TODO !!! think of how to facilitate [[variable lengths]] with 'extra length trick'
+
+  trait CircularArrayFilterImpl[S] extends TFilterImpl[S] {
+    private[tsynth] val arr:Array[ElemType]
+    private[tsynth] var head = 0
+    private[tsynth] var tail = length - 1
+
+    //T0D0 given some [array-specific] function from a [subclass], operate should be definable already
+    //  - Do I want the very initial array to be played as such at all? If not, how do I order the
+    //    operations? -- hm -- if readSources are done first, would that even be avoidable at all?
+    //    - Let it happen. It has more info anyway, so I can have things both ways, adding fixes of top if need
+
+    private[tsynth] def operate {
+      arr(head) = headUpdate(lastRead, arr, head, tail)
+      head = (head + 1) % length
+      tail = (tail + 1) % length
+    }
+    private[tsynth] def value = arr(head)
+  
+    /*
+    //T0d0 think:  what should the visibility on this be, if it's to be user-supplied? same question goes
+    //      for other such things too, so go through them at some point!
+    private[tsynth] def headUpdate( .... */
+  }
 }
 
 
